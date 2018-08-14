d6d680d4c19db5eafb4e68986a669caf7dc531b3
Hyung Lee and Meria Thomas <meria@>
2018-08-14 10:14:15 -0700

    Finish HTML and JavaScript.

---
+++ js/scripts.js
@@ -0,0 +1,30 @@
+//Back-end
+function replaceVowels(array) {
+  for (i = 0; i < array.length; i++) {
+    if (array[i] === "a" || array[i] === "e" || array[i] === "i" || array[i] === "o" || array[i] === "u") {
+      array[i] = "-";
+    }
+  }
+}
+
+
+//User Interface
+$(document).ready(function() {
+  var inputArray = [];
+  $("#formOne").submit(function(event){
+    var userInput = $("#inputBox").val();
+    inputArray = userInput.split("");
+
+    replaceVowels(inputArray);
+
+    var puzzleString = inputArray.join("");
+    $("#formOne").hide();
+    $(".results").slideDown();
+
+    $("#outputResult").text(puzzleString);
+
+    event.preventDefault();
+  });
+
+
+});
